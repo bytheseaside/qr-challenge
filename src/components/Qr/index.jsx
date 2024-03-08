9c095fbd5dbc85d41c938afbe67fb1905ed1eb8c
Brisa Rojas <rojaasbrisa@gmail.com>
2024-03-08 17:01:36 -0300

    fix color

--- src/components/Qr/index.jsx
+++ src/components/Qr/index.jsx
@@ -2,13 +2,13 @@ import QrCode from "./QrCode";
 
 const Qr = () => (
   <div className="relative overflow-hidden mb-6 rounded-[10px]">
-    <div className="bg-blue-dark rounded-full z-0 absolute w-80 h-80 -top-[175px] -left-[115px]" />
+    <div className="bg-blue  rounded-full z-0 absolute w-80 h-80 -top-[175px] -left-[115px]" />
     <div
-      className="bg-blue p-16 w-72 h-72  flex-initial"
+      className="bg-blue-dark p-16 w-72 h-72  flex-initial"
     >
       <QrCode />
     </div>
-    <div className="bg-blue-dark rounded-full z-0 absolute w-[270px] h-[270px] top-[204.5px] -right-[113.5px]" />
+    <div className="bg-blue rounded-full z-0 absolute w-[270px] h-[270px] top-[204.5px] -right-[113.5px]" />
   </div>
 )
 
